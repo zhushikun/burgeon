bbd065fc25fc547ec56ff25c6194a9e4d6e265da
zhushikun <shikun.zhu@dianrong.com>
2017-09-25 12:04:27 +0800

    m map

---
+++ src/main/java/com/lwrs/app/db/mapper/InviteMapper.java
@@ -0,0 +1,35 @@
+package com.lwrs.app.db.mapper;
+
+import com.lwrs.app.db.entity.InviteDB;
+import com.lwrs.app.db.sql.InviteSql;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+public interface InviteMapper {
+
+    @Insert({"<script>",
+        InviteSql.INSERT,
+        "</script>"
+    })
+    @Options(useGeneratedKeys = true)
+    Long insert(@Param("pojo") InviteDB pojo);
+
+    @Update(InviteSql.UPDATE_BY_ID)
+    int updateById(@Param("pojo") InviteDB pojo);
+
+
+    @Select(InviteSql.SELECT_BY_ID)
+    InviteDB selectById(@Param("id") Long id);
+
+    @Select(InviteSql.SELECT_BY_MASTER_ID)
+    List<InviteDB> selectByMasterId(@Param("materId") Long materId);
+
+    @Select(InviteSql.SELECT_BY_GUEST_ID)
+    List<InviteDB> selectByGuestId(@Param("guestId") Long guestId);
+
+}
---
+++ src/main/java/com/lwrs/app/db/mapper/WxOauthMapper.java
@@ -0,0 +1,34 @@
+package com.lwrs.app.db.mapper;
+
+import com.lwrs.app.db.entity.WxOauthDB;
+import com.lwrs.app.db.sql.WxOauthSql;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+public interface WxOauthMapper {
+
+    @Insert({"<script>",
+        WxOauthSql.INSERT,
+        "</script>"
+    })
+    @Options(useGeneratedKeys = true)
+    Long insert(@Param("pojo") WxOauthDB pojo);
+
+    @Update(WxOauthSql.UPDATE_BY_ID)
+    int updateById(@Param("pojo") WxOauthDB pojo);
+
+    @Select(WxOauthSql.SELECT_BY_ID)
+    WxOauthDB selectById(@Param("id") Long id);
+
+
+    @Select(WxOauthSql.SELECT_BY_USERID)
+    WxOauthDB selectByUserId(@Param("userId") Long userId);
+
+
+    @Select(WxOauthSql.SELECT_BY_OPENID)
+    WxOauthDB selectByOpenId(@Param("openId") String openId);
+
+}
